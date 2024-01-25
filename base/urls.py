71a2cea55714c74fbb98ed878f87c7f9a9e3980f
mdnaimur <mdnaimurr@gmail.com>
2024-01-25 16:18:40 +0600

    user registration and login

--- base/urls.py
+++ base/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('registration/',views.EmployeesRegistrationFromView.as_view(),name='user_registration'),
+    path('login/',views.UserLogin,name='login'),
+    path('logout/',views.UserLogOut,name='logout'),
 ]
